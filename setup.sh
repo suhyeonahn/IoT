02f485debbb6e226c9187116e3b74480c013a97c
Suhyeon AHN <54574221+suhyeonahn@users.noreply.github.com>
2023-08-15 23:10:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade
-sudo apt install vim git openssl virtualbox -y
+sudo apt install vim git openssh-server virtualbox -y
 virtualbox
 wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb
